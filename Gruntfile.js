c05d65e3c14f295fd1f31d63697c26fb919258b9
Albino Tonnnina <albino.tonnina@workshare.com>
2017-08-17 11:24:09 +0100

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
 
         connect: {
             options: {
-                port: 5000,
+                port: 3000,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
