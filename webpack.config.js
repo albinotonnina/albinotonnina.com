9cd71291799565721cf161d731e1bbab45617afa
Albino Tonnnina <albino.tonnina@workshare.com>
2017-08-28 13:04:03 +0100

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,130 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const ModernizrWebpackPlugin = require('modernizr-webpack-plugin');
-var StringReplacePlugin = require("string-replace-webpack-plugin");
+const StringReplacePlugin = require("string-replace-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const extractSass = new ExtractTextPlugin({
-    filename: "styles.css"
-});
+module.exports = function (env = {}) {
 
-module.exports = {
-    devtool: 'source-map',
-    entry: './src/scripts/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        extractSass,
-        // new webpack.optimize.UglifyJsPlugin(),
-        new HtmlWebpackPlugin({template: './src/index.hbs'}),
-        new ModernizrWebpackPlugin({
-            'feature-detects': [
-                'svg'
-            ]
-        }),
-        new StringReplacePlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.hbs$/,
-                loader: 'handlebars-loader',
-                query: {
-                    partialDirs: [
-                        path.join(__dirname, 'src')
-                    ],
-                    inlineRequires: '/images/'
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }]
-                })
-            },
+    const extractSass = new ExtractTextPlugin({
+        filename: "styles.css"
+    });
 
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader'
-                    }
+    const plugins = (() => {
+
+        const pluginsArray = [
+            extractSass,
+
+            new CleanWebpackPlugin('build'),
+            new HtmlWebpackPlugin({template: './src/index.hbs'}),
+            new ModernizrWebpackPlugin({
+                'feature-detects': [
+                    'svg'
                 ]
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader?classPrefix'
-            },
-            {
-                test: /\.svg$/,
-                loader: StringReplacePlugin.replace({
-                    replacements: [
+            }),
+            new StringReplacePlugin()
+        ];
+
+        if (env.production) {
+            pluginsArray.push(new webpack.optimize.UglifyJsPlugin());
+
+        }
+
+        return pluginsArray;
+
+    })();
+
+    return {
+        devtool: 'source-map',
+        entry: './src/scripts/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bundle.js'
+        },
+        plugins,
+        devServer: {
+            contentBase: path.join(__dirname, "build"),
+            compress: true,
+            port: 4000,
+            host: '0.0.0.0',
+            open: true,
+            inline: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.hbs$/,
+                    loader: 'handlebars-loader',
+                    query: {
+                        partialDirs: [
+                            path.join(__dirname, 'src')
+                        ],
+                        inlineRequires: '/images/'
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: extractSass.extract({
+                        use: [{
+                            loader: "raw-loader"
+                        }, {
+                            loader: "sass-loader"
+                        }]
+                    })
+                },
+
+                {
+                    test: /\.html$/,
+                    use: ["html-loader"]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
                         {
-                            pattern: /font-family="'Roboto-Thin'"/ig,
-                            replacement: function (match, p1, offset, string) {
-                                return 'font-weight="100"';
-                            }
-                        },
-                        {
-                            pattern: /font-family="'Roboto-Light'"/ig,
-                            replacement: function (match, p1, offset, string) {
-                                return 'font-weight="300"';
-                            }
-                        },
-                        {
-                            pattern: /font-family="'Roboto-Regular'"/ig,
-                            replacement: function (match, p1, offset, string) {
-                                return 'font-weight="400"';
-                            }
-                        },
-                        {
-                            pattern: /font-family="'Roboto-Black'"/ig,
-                            replacement: function (match, p1, offset, string) {
-                                return 'font-weight="900"';
-                            }
+                            loader: 'file-loader'
                         }
                     ]
-                })
-            }
-        ]
+                },
+                {
+                    test: /\.svg$/,
+                    loader: 'svg-inline-loader?classPrefix'
+                },
+                {
+                    test: /\.svg$/,
+                    loader: StringReplacePlugin.replace({
+                        replacements: [
+                            {
+                                pattern: /font-family="'Roboto-Thin'"/ig,
+                                replacement: () => 'font-weight="100"'
+                            },
+                            {
+                                pattern: /font-family="'Roboto-Light'"/ig,
+                                replacement: () => 'font-weight="300"'
+                            },
+                            {
+                                pattern: /font-family="'Roboto-Regular'"/ig,
+                                replacement: () => 'font-weight="400"'
+                            },
+                            {
+                                pattern: /font-family="'Roboto-Black'"/ig,
+                                replacement: () => 'font-weight="900"'
+                            }
+                        ]
+                    })
+                }
+            ]
+        }
     }
 };
