67c27f174c47e386dae382438baaac030032c295
Albino Tonnina <albinotonnina@gmail.com>
2025-11-18 00:08:09 +0000

    Add @vercel/analytics dependency to package.json

--- developer-playbook-ai-era/src/main.jsx
+++ developer-playbook-ai-era/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Analytics } from "@vercel/analytics/next"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
